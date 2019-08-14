ad50e28e7fa323d98ff4f771647a8fa77ab4ab20
Osh2314 <osh231426@gmail.com>
2019-08-15 04:10:56 +0900

    어새신 폼 1

---
+++ Assets/Scripts/Object/Enemy/Enemy_Assassin.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Assassin : Enemy
+{
+    // Start is called before the first frame update
+    public GameObject Knife;
+    public float knifespeed;
+
+    Enemy_Assassin_Knife knife;
+    public int atkpoint = 0;
+
+    // Start is called before the first frame update
+    new void Start()
+    {
+        knife = GetComponent<Enemy_Assassin_Knife>();
+        base.Start();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override IEnumerator State_Attack()
+    {
+        state = State.ATTACK;
+        atkpoint++;
+        while (state == State.ATTACK)
+        {
+            if (atkpoint >= 2)
+            {
+                atkpoint--;
+                yield break;
+            }
+            GameObject createdObject;
+            Rigidbody2D createdRigid;
+            Vector2 lookDir = playerPos - transform.position;
+            lookDir.Normalize();
+            createdObject = Instantiate(Knife, new Vector3(transform.position.x + 0.3f, transform.position.y, transform.position.z), Quaternion.FromToRotation(Vector3.right, lookDir));
+            createdRigid = createdObject.GetComponent<Rigidbody2D>();
+            createdRigid.AddForce(lookDir * knifespeed);
+            yield return new WaitForSeconds(3f);
+        }
+        atkpoint--;
+        yield break;
+    }
+}
---
+++ Assets/Scripts/Object/Enemy/Range/AssassinRange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssassinRange : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    Enemy enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy = GetComponentInParent<Enemy>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = Enemy.gameObject.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && enemy.state == global::Enemy.State.MOVE)
+        {
+            if(enemy.gameObject.transform.position.x >= collision.transform.position.x)
+            {
+            enemy.gameObject.transform.position = new Vector3(collision.transform.position.x  - 1, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
+            else
+            {
+                enemy.gameObject.transform.position = new Vector3(collision.transform.position.x + 1, gameObject.transform.position.y, gameObject.transform.position.z);
+            }
+
+        }
+    }
+}
