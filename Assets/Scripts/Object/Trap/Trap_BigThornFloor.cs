923079fee198d5a95acc2828ea95875400127eea
Osh2314 <osh231426@gmail.com>
2019-08-18 02:45:18 +0900

    가시함정(약간) 추가

---
+++ Assets/Scripts/Object/Trap/Trap_BigThornFloor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_BigThornFloor : MonoBehaviour
+{
+    GameObject Thron1;
+    GameObject Thron2;
+    GameObject Thron3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Thorn()
+    {
+        while(true)
+        {
+            Thron1.gameObject.SetActive(false);
+            Thron2.gameObject.SetActive(false);
+            Thron3.gameObject.SetActive(false);
+            yield return new WaitForSeconds(3f);
+            Thron1.gameObject.SetActive(true);
+            Thron2.gameObject.SetActive(true);
+            Thron3.gameObject.SetActive(true);
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
